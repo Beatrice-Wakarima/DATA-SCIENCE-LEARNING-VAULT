03391db7acd2c14297e662bb8b49eb312c6ba1d9
Beatrice Wakarima <beatiewakarima1@gmail.com>
2026-05-19 08:37:10 +0300

    Update site title

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Beatrice W Wanjiru Garden",
+    pageTitle: "Welcome to my Garden",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
